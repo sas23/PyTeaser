c404b7a6ed950475f293dda3cbe2acc5fd98e8d0
Stephen Stoyles <stephens@signiq.com>
2014-08-13 19:43:12 +0800

    removed non ascii characters from scraped text

--- pyteaser.py
+++ pyteaser.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from collections import Counter
 from math import fabs
+import string
 from re import split as regex_split, sub as regex_sub, UNICODE as REGEX_UNICODE
 
 stopWords = set([
